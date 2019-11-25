1f4621da3404d6af16a9e3341d32833fe19b24a0
Eric Mesa <ericsbinaryworld@gmail.com>
2019-11-24 19:04:46 -0500

    Moves left and right

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    Rigidbody2D myRigidboy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidboy = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal");  //value is -1 to 1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidboy.velocity.y);
+        myRigidboy.velocity = playerVelocity;
+    }
+}
