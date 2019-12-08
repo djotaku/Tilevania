c6b7a55ddfaef5540fd6d77e47485ee2d4b4f2eb
Eric Mesa <ericsbinaryworld@gmail.com>
2019-12-07 21:12:37 -0500

    coins stay gone when you die
    Also added a new level for last lesson

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour
+{
+
+    int startingSceneIndex;
+    
+    private void Awake()
+    {
+        int numScenePersist = FindObjectsOfType<ScenePersist>().Length;
+        if(numScenePersist > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void Update()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex != startingSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
